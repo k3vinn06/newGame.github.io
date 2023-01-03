c6dfabacd211c02b241f00c14e2b894dea849b47
k3vinn06 <115999511+k3vinn06@users.noreply.github.com>
2023-01-03 14:04:11 +0000

    Add files via upload

---
+++ Enemy.js
@@ -0,0 +1,35 @@
+export default class Enemy {
+    constructor(x, y, color, health) {
+      this.x = x;
+      this.y = y;
+      this.color = color;
+      this.health = health;
+      this.width = 50;
+      this.height = 50;
+    }
+  
+    draw(ctx) {
+      ctx.fillStyle = this.color;
+      if (this.health > 0 ) {
+        ctx.strokeStyle = "black";
+      } else {
+        ctx.strokeStyle = this.color;
+      }
+  
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+      ctx.strokeRect(this.x, this.y, this.width, this.height);
+  
+     
+      ctx.fillStyle = "black";
+      ctx.font = "25px Arial";
+      ctx.fillText(
+        this.health,
+        this.x + this.width / 2.75,
+        this.y + this.height / 1.5
+      );
+    }
+  
+    takeDamage(damage) {
+      this.health -= damage;
+    }
+  }
---
+++ Player.js
@@ -0,0 +1,92 @@
+export default class Player {
+    constructor(x, y, bulletController) {
+      this.x = x;
+      this.y = y;
+      this.bulletController = bulletController;
+      this.width = 50;
+      this.height = 50;
+      this.speed = 4;
+      
+
+      document.addEventListener("keydown", this.keydown);
+      document.addEventListener("keyup", this.keyup);
+    }
+  
+    draw(ctx) {
+      this.move();
+      ctx.strokeStyle = "white";
+      ctx.strokeRect(this.x, this.y, this.width, this.height);
+      ctx.fillStyle = "black";
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+
+      
+  
+      this.shoot();
+    }
+  
+    shoot() {
+      if (this.shootPressed) {
+        const speed = 5;
+        const delay = 7;
+        const damage = 1;
+        const bulletX = this.x + this.width / 2;
+        const bulletY = this.y;
+        this.bulletController.shoot(bulletX, bulletY, speed, damage, delay);
+      }
+    }
+  
+    move() {
+      if (this.downPressed) {
+        this.y += this.speed;
+      }
+      if (this.upPressed) {
+        this.y -= this.speed;
+      }
+      if (this.leftPressed) {
+        this.x -= this.speed;
+      }
+  
+      if (this.rightPressed) {
+        this.x += this.speed;
+      }
+    }
+  
+    keydown = (e) => {
+      if (e.code === "KeyW" ) {
+        this.upPressed = true;
+      }
+      if (e.code === "KeyS" ) {
+        this.downPressed = true;
+      }
+      if (e.code === "KeyA") {
+        this.leftPressed = true;
+      }
+      if (e.code === "KeyD") {
+        this.rightPressed = true;
+      }
+      if (e.code === "Space") {
+        this.shootPressed = true;
+      }
+    };
+  
+    keyup = (e) => {
+      if (e.code === "KeyW"  ) {
+        this.upPressed = false;
+      }
+      if (e.code === "KeyS" ) {
+        this.downPressed = false;
+      }
+      if (e.code === "KeyA") {
+        this.leftPressed = false;
+      }
+      if (e.code === "KeyD") {
+        this.rightPressed = false;
+      }
+      if (e.code === "Space") {
+        this.shootPressed = false;
+      }
+    };
+
+
+
+  }
